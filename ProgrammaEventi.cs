da645cf39f0f325561dd15862dfc4ab70d1d3f92
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-27 16:13:40 +0200

    Add classe ProgrammaEventi

--- ProgrammaEventi.cs
+++ ProgrammaEventi.cs
@@ -8,6 +8,37 @@ namespace GestoreEventi
 {
     public class ProgrammaEventi
     {
+        string titolo;
+        List<Evento> eventi;
 
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento evento)
+        {
+            eventi.Add(evento);
+        }
+
+        public List<Evento> TrovaEvento(DateTime data)
+        {
+            var filteredEvents = new List<Evento>();
+
+            foreach (Evento evento in eventi)
+            {
+                if(evento.getData() == data)
+                {
+                    filteredEvents.Add(evento);
+                }
+                else
+                {
+                    Console.WriteLine("Nessun evento in programma per questa data!");
+                }
+            }
+
+            return filteredEvents;
+        }
     }
 }
